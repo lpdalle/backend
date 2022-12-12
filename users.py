9733c0432a621804d4548d1d68bb8fc753d8d297
Artem Voskresensky <metravs@gmail.com>
2022-12-12 20:51:18 +0300

    added storage

--- users.py
+++ users.py
@@ -1,39 +1,30 @@
 from dataclasses import dataclass, asdict
 from flask import Flask, request
+from storage import UserStorage
 from uuid import uuid4
 
 app = Flask(__name__)
 
-@dataclass
-class Users:
-    uid: str
-    login: str
-    email: str
-
-users = {}
-
+users = UserStorage()
 
 @app.get('/api/v1/users/')
 def get_all():
-    return list(users.values())
+    return users.get_all()
 
 
 @app.get('/api/v1/users/<string:uid>')
 def get_by_uid(uid: str):
-    if users.get(uid):
-        return asdict(users[uid])
+    if users.get_by_uid(uid):
+        return users.get_by_uid(uid)
     return {}, 404
 
 
 @app.post('/api/v1/users/')
 def add():
-    uid = uuid4().hex
     user = request.json
-    user['uid'] = uid
     user_login = request.json["login"]
     user_email = request.json["email"]
-    new_user = Users(uid=user['uid'], login=user_login, email=user_email)
-    users[new_user.uid] = new_user
+    users.add(login=user_login, email=user_email)
     return user, 201
 
 
@@ -41,14 +32,14 @@ def add():
 def update(uid: str):
     user_login = request.json["login"]
     user_email = request.json["email"]
-    users[uid] = Users(uid=uid, login=user_login, email=user_email)
-    return asdict(users[uid]), 201
+    users.update(uid=uid, login=user_login, email=user_email)
+    return users.get_by_uid(uid), 201
 
 
 @app.delete('/api/v1/users/<string:uid>')
 def delete(uid):
-    if users.get(uid):
-        del users[uid]
+    if users.get_by_uid(uid):
+        users.delete(uid)
         return {}, 204
     return {}, 404
 
