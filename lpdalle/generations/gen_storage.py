dd8b4904a972492f7f17072fe2216383f5b18f78
Artem Voskresensky <metravs@gmail.com>
2022-12-27 02:44:22 +0300

    add endpoint for generation add, refactoring

--- lpdalle/generations/gen_storage.py
+++ lpdalle/generations/gen_storage.py
@@ -11,9 +11,9 @@ class GenerationsStorage:
 
         return gen
 
-    def add(self, user_uid, promt, status):
+    def add(self, user_id: int, prompt: str, status: str) -> Generations:
         uid = None
-        new_generation = Generations(uid=uid, user_uid=user_uid, promt=promt, status=status)
+        new_generation = Generations(uid=uid, user_id=user_id, prompt=prompt, status=status)
         db_session.add(new_generation)
         db_session.commit()
         return new_generation
--- lpdalle/generations/generations.py
+++ lpdalle/generations/generations.py
@@ -1,5 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, request
+from sqlalchemy.exc import IntegrityError
 
+from lpdalle.errors import BadRequestError, ConflictError
 from lpdalle.generations.gen_storage import GenerationsStorage
 from lpdalle.schemas import Generations
 
@@ -8,8 +10,33 @@ view_generations = Blueprint('generations', __name__)
 generations_storage = GenerationsStorage()
 
 
-@view_generations.get('/<int: uid>')
+@view_generations.get('/<int:uid>')
 def get_by_uid(uid: int):
     get_generation = generations_storage.get_by_uid(uid)
     generation = Generations.from_orm(get_generation)
     return generation.dict()
+
+
+@view_generations.post('/')
+def add():
+    try:
+        generation = request.json
+    except BadRequestError:
+        raise BadRequestError('message')
+
+    if not generation:
+        raise BadRequestError('Empty generation data!')
+
+    generation['uid'] = -1
+    new_generation = Generations(**generation)
+
+    try:
+        generation_add = generations_storage.add(
+            user_id=new_generation.user_id,
+            prompt=new_generation.prompt,
+            status=new_generation.status,
+        )
+    except IntegrityError:
+        raise ConflictError('generations', new_generation.uid)
+    new_gen_add = Generations.from_orm(generation_add)
+    return new_gen_add.dict(), 201
