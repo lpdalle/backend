a507d3be2bc1cf566bc929ce481326ad7dc76492
Artem Voskresensky <metravs@gmail.com>
2023-01-17 00:35:13 +0300

    refactoring

--- lpdalle/__main__.py
+++ lpdalle/__main__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from pydantic import ValidationError
 
+from lpdalle.config import PORT
 from lpdalle.db import db_session
 from lpdalle.errors import AppError
 from lpdalle.generation.view import view_generation, view_user_generations
@@ -38,7 +39,7 @@ def main() -> None:
     app.register_error_handler(ValidationError, handle_validation_error)
     app.register_error_handler(400, handle_emptystring_error)  # noqa: WPS432
     app.teardown_appcontext(shutdown_session)
-    app.run()
+    app.run(host='0.0.0.0', port=PORT)
 
 
 if __name__ == '__main__':
--- lpdalle/config.py
+++ lpdalle/config.py
@@ -1,3 +1,4 @@
 import os
 
 ELEPHANT_URL = os.environ['ELEPHANT_URL']
+PORT = int(os.environ['PORT'])
